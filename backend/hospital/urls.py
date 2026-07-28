45a5e3b491a828dff2f261f2470c4f7664c08e7b
sumit12312299 <sumitkr4364@gmail.com>
2026-07-28 23:46:16 +0530

    refactor(backend): add app_name namespace definition in hospital/urls.py

--- backend/hospital/urls.py
+++ backend/hospital/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
+app_name = 'hospital'
+
 urlpatterns = [
+
     # JWT Auth
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
