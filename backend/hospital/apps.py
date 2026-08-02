5a7429aeb8fdfa36b025a9a0fcbdc18a85df3b2d
sumit12312299 <sumitkr4364@gmail.com>
2026-08-02 16:22:50 +0530

    docs: Add setup comments to Django AppConfig class properties

--- backend/hospital/apps.py
+++ backend/hospital/apps.py
@@ -7,6 +7,7 @@ from django.apps import AppConfig
 
 class HospitalConfig(AppConfig):
     """Configuration class for the Hospital Django application."""
+    # Define primary key auto-increment field type to avoid migration deprecations.
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hospital'
 
